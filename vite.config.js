82554b2f8ef978b71091a40176cc1b04a39a749a
RomainMarceDesj <desjonqueres@keio.jp>
2025-09-05 11:01:50 +0900

    hore: update package.json and vite config, remove unused requirements.txt

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/read2learnkanji",
+  base:"/read2learnkanji/",
 })
